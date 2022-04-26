ca3f89dfca60d175c9630a179dbbfffd2b803cd1
Roohollah Etemadi <roohollah.etemadi@amd.com>
2022-04-26 19:09:42 +0000

    Created README, docker and singularity run scripts for AMD Infinity Hub user guide for OpenMM

---
+++ openmm/run_openmm_singularity.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+#************************************************************************************************
+# Usage: ./run_openmm_singularity.sh                                                            *
+# Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.                          *
+# This script is used to run AMD Infiniy Openmm singularity commands                            *
+# Contact info: roohollah.etemadi@amd.com                                                       *
+# Version: V1.0                                                                                 * 
+# Modified: 2022-04-27                                                                          *
+# Version History:                                                                              *
+# V1.0: run amdih/openmm:7.7.0_49 singularity                                                   *
+#************************************************************************************************
+
+echo "Date: `date`"
+
+echo "==== Pull singularity image ===="
+echo "singularity pull openmm.sif docker://amdih/openmm:7.7.0_49"
+singularity pull openmm.sif docker://amdih/openmm:7.7.0_49
+echo "==== Pull complete ===="
+
+echo "==== Run singulariy test ===="
+echo "singularity run ./openmm.sif run-benchmarks"
+singularity run ./openmm.sif run-benchmarks 
+echo "==== Run complete ====" 
