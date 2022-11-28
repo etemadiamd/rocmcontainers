1f7930d07c8b45933f1659e99712a6765258a937
Roohollah Etemadi <roohollah.etemadi@amd.com>
2022-11-28 14:39:56 +0000

    removed privileged option

--- rochpl/run_rochpl_podman.sh
+++ rochpl/run_rochpl_podman.sh
@@ -13,23 +13,23 @@
 echo "Start Test:`date`"
 
 echo "1 gpu"
-echo "podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 1 -Q 1 -N 90112 --NB 512"
-podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 1 -Q 1 -N 90112 --NB 512"
+echo "podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 1 -Q 1 -N 90112 --NB 512"
+podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 1 -Q 1 -N 90112 --NB 512"
 
 echo "2 gpu"
-echo "podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 2 -Q 1 -N 128000 --NB 512"
-podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 2 -Q 1 -N 128000 --NB 512"
+echo "podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 2 -Q 1 -N 128000 --NB 512"
+podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 2 -Q 1 -N 128000 --NB 512"
 
 echo "4 gpu"
-echo "podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 2 -Q 2 -N 180224 --NB 512"
-podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 2 -Q 2 -N 180224 --NB 512"
+echo "podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 2 -Q 2 -N 180224 --NB 512"
+podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 2 -Q 2 -N 180224 --NB 512"
 
 echo "8 gpu"
-echo "podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 2 -Q 4 -N 256000 --NB 512"
-podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 2 -Q 4 -N 256000 --NB 512"
+echo "podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 2 -Q 4 -N 256000 --NB 512"
+podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 2 -Q 4 -N 256000 --NB 512"
 
 echo "16 gpu"
-echo "podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 4 -Q 4 -N 360448 --NB 512"
-podman run -it --privileged --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 4 -Q 4 -N 360448 --NB 512"
+echo "podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c mpirun_rochpl -P 4 -Q 4 -N 360448 --NB 512"
+podman run -it --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 amdih/rochpl:5.0.5_49 /bin/bash -c "mpirun_rochpl -P 4 -Q 4 -N 360448 --NB 512"
 
 echo "End Test:`date`"
