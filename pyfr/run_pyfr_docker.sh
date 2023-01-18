3eb7c2ac68079e9c465d0fe53b3f5dab4fb5991e
Anand <anand.raghavendra@amd.com>
2023-01-17 19:50:02 -0500

    Updated the container version to the latest

--- pyfr/run_pyfr_docker.sh
+++ pyfr/run_pyfr_docker.sh
@@ -6,8 +6,9 @@
 # Contact info: roohollah.etemadi@amd.com                                                       
 # Contact info: anand.raghavendra@amd.com                                   
 # Version: V1.1                                                                                  
-# Modified: 2023-01-14                                                                          
-# Version History:                                                                              
+# Modified: 2023-01-17                                                                          
+# Version History:
+# V1.2: added the latest docker pull tag                                                                               
 # V1.1: added the docker commands for NACA0021 benchmark 
 # Version: V1.0                                                                                  
 # Modified: 2022-05-26                                                                          
@@ -17,27 +18,27 @@
 echo "Start PyFR:`date`"
 
 echo "==== Pull docker image  ===="
-echo "docker pull amdih/pyfr:1.13.0_44"
-docker pull amdih/pyfr:1.13.0_44
+echo "docker pull amddcgpuce/pyfr:1.15.amd1_54"
+docker pull amddcgpuce/pyfr:1.15.amd1_54
 echo "==== Pull complete  ===="
 
 echo "==== Run BSF, TGV and NACA0021 Benchmarks on 1 GPU ===="
-echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c \"run-benchmark BSF --ngpus 1\""
-docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c "run-benchmark BSF --ngpus 1"
+echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c \"run-benchmark BSF --ngpus 1\""
+docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c "run-benchmark BSF --ngpus 1"
 
-echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c \"run-benchmark tgv --ngpus 1\""
-docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c "run-benchmark tgv --ngpus 1"
+echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c \"run-benchmark tgv --ngpus 1\""
+docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c "run-benchmark tgv --ngpus 1"
 
-echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c \"run-benchmark naca0021 --ngpus 1\""
-docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c "run-benchmark naca0021 --ngpus 1"
+echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c \"run-benchmark naca0021 --ngpus 1\""
+docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c "run-benchmark naca0021 --ngpus 1"
 echo "==== Completed tests on 1 GPU ===="
 
 echo "==== Run TGV and NACA0021 Benchmarks on 2 GPUs ===="
-echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c \"run-benchmark tgv --ngpus 2\""
-docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c "run-benchmark tgv --ngpus 2"
+echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c \"run-benchmark tgv --ngpus 2\""
+docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c "run-benchmark tgv --ngpus 2"
 
-echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c \"run-benchmark naca0021 --ngpus 2\""
-docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/pyfr:1.13.0_44 /bin/bash -c "run-benchmark naca0021 --ngpus 2"
+echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c \"run-benchmark naca0021 --ngpus 2\""
+docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amddcgpuce/pyfr:1.15.amd1_54 /bin/bash -c "run-benchmark naca0021 --ngpus 2"
 echo "==== Completed Test on 2 GPUs ===="
 
 echo "End Test:`date`"
--- specfem3dcartesian/run_specfem3dcartesian_docker.sh
+++ specfem3dcartesian/run_specfem3dcartesian_docker.sh
@@ -3,36 +3,37 @@
 # Usage: sudo sh run_specfem3dcartesian_docker.sh                                                        
 # Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.                         
 # This script is used to run AMD Infiniy SPECFEM3D Cartesian Docker Container                                
-# Contact info: roohollah.etemadi@amd.com                                                       
-# Version: V1.0                                                                                  
-# Modified: 2022-05-17                                                                          
-# Version History:                                                                              
+# Contact info: roohollah.etemadi@amd.com; anand.raghavendra@amd.com                                                       
+# Version: V1.1                                                                                  
+# Modified: 2023-01-17                                                                          
+# Version History:
+# V1.1: run amddcgpuce/specfem3d:2022.11.amd2_41 Docker image                                                                               
 # V1.0: run amdih/specfem3d:9c0626d1-20201122 Docker image                                             
 #************************************************************************************************
 echo "Start Test:`date`"
 
-echo "==== Pull Chroma docker image  ===="
-echo "docker pull amdih/specfem3d:9c0626d1-20201122"
-docker pull amdih/specfem3d:9c0626d1-20201122
+echo "==== Pull SPECFEM3D Cartesian docker image  ===="
+echo "docker pull amddcgpuce/specfem3d:2022.11.amd2_41"
+docker pull amddcgpuce/specfem3d:2022.11.amd2_41
 echo "==== Pull complete  ===="
 
 echo "==== Run test on 1 GPU"
-echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 1 -c 0 -s 288x256\""
-docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 1 -c 0 -s 288x256"
+echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 1 -c 0 -s 288x256\""
+docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 1 -c 0 -s 288x256"
 echo "==== Complete test on 1 GPU ===="
 
 echo "==== Run test on 2 GPUs"
-echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 2 -c 0 -s 288x256\""
-docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 2 -c 0 -s 288x256"
+echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 2 -c 0 -s 288x256\""
+docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 2 -c 0 -s 288x256"
 echo "==== Complete test on 2 GPUs ===="
 
 echo "==== Run test on 4 GPUs"
-echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 4 -c 0 -s 288x256\""
-docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 4 -c 0 -s 288x256"
+echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 4 -c 0 -s 288x256\""
+docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 4 -c 0 -s 288x256"
 echo "==== Complete test on 4 GPUs ===="
 
 echo "==== Run test on 8 GPUs"
-echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 8 -c 0 -s 288x256\""
-docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amdih/specfem3d:9c0626d1-20201122 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 8 -c 0 -s 288x256"
+echo "docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c \"export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 8 -c 0 -s 288x256\""
+docker run --rm -it --ipc=host --device=/dev/dri --device=/dev/kfd --security-opt seccomp=unconfined amddcgpuce/specfem3d:2022.11.amd2_41 /bin/bash -c "export OMPI_ALLOW_RUN_AS_ROOT=1; export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1; benchmark cartesian -g 8 -c 0 -s 288x256"
 echo "==== Complete test on 8 GPUs ===="
 
