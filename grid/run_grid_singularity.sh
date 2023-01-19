3480be030e0ce53887e0f9d5917ea9de45bfece5
Sanjay Tripathi <Sanjay.Tripathi@amd.com>
2023-01-19 13:45:11 -0800

    added docker tag as parameter, added timestamp after every benchmark run

--- grid/run_grid_singularity.sh
+++ grid/run_grid_singularity.sh
@@ -1,39 +1,58 @@
 #!/bin/sh -x
 #************************************************************************************************
-# Usage: sh run_grid_singularity.sh                                                           
-# Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.                        
-# This script is used to run AMD Infiniy GRID singularity commands                            
-# Contact info: roohollah.etemadi@amd.com                                                      
-# Version: V1.0                                                                                  
-# Modified: 2022-05-18                                                                          
-# Version History:                                                                              
-# V1.0: run amdih/grid:0.8.2-20211213 singularity commands                                   
+# Usage: sh run_grid_singularity.sh
+# Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# This script is used to run AMD Infiniy GRID singularity commands
+# Contact info: roohollah.etemadi@amd.com, sanjay.tripathi@amd.com
+# Version: V1.1
+# Modified: 2023-01-19
+# Version History:
+# V1.1: updated docker-tag as parameter, added timestamp after every benchmark run
+# V1.0: run amdih/grid:0.8.2-20211213 singularity commands
 #************************************************************************************************
 echo "Start Test: `date`"
 
+# Use docker-tag, if provided as parameter else exit
+if [ "$#" -eq 0 ];
+        then
+                echo "Need Docker-Tag. Exiting."
+                exit 1
+        else
+		tag=$1
+fi
+
+DOCKER_TAG=grid:$tag
+SIF_FILENAME=grid_$tag.sif
+
 echo "==== Pull singularity image ===="
-echo "singularity pull grid.sif docker://amdih/grid:0.8.2-20211213"
-singularity pull grid.sif docker://amdih/grid:0.8.2-20211213
+echo "singularity pull $SIF_FILENAME docker://amddcgpuce/$DOCKER_TAG"
+singularity pull --docker-login $SIF_FILENAME docker://amddcgpuce/$DOCKER_TAG
 echo "==== Pull complete ===="
 
 echo "==== Run Test on 1 GPU ===="
-echo "singularity run grid.sif Benchmark_ITT --accelerator-threads 8"
-singularity run grid.sif Benchmark_ITT --accelerator-threads 8
-echo "==== Complete Test on 1 GPU ===="
+echo "singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 1 gpu_bind.sh Benchmark_ITT --accelerator-threads 8"
+singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 1 gpu_bind.sh Benchmark_ITT --accelerator-threads 8
+echo "==== Completed Test on 1 GPU ===="
+
+echo "Date: `date`"
 
 echo "==== Run Test on 2 GPUs ===="
-echo "singularity run grid.sif mpirun -np 2  /benchmark/gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.2"
-singularity run grid.sif mpirun -np 2  /benchmark/gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.2
-echo "==== Complete Test on 2 GPUs ===="
+echo "singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 2 gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.2"
+singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 2 gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.2
+echo "==== Completed Test on 2 GPUs ===="
+
+echo "Date: `date`"
 
 echo "==== Run Test on 4 GPUs ===="
-echo "singularity run grid.sif mpirun -np 4  /benchmark/gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.4"
-singularity run grid.sif mpirun -np 4  /benchmark/gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.4
-echo "==== Complete Test on 4 GPUs ===="
+echo "singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 4 gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.4"
+singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 4 gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.4
+echo "==== Completed Test on 4 GPUs ===="
+
+echo "Date: `date`"
 
 echo "==== Run Test on 8 GPUs ===="
-echo "singularity run grid.sif mpirun -np 8  /benchmark/gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.8"
-singularity run grid.sif mpirun -np 8  /benchmark/gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.8
-echo "==== Complete Test on 8 GPUs ===="
+echo "singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 8 gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.8"
+singularity run --pwd /benchmark $SIF_FILENAME mpirun -np 8 gpu_bind.sh Benchmark_ITT --accelerator-threads 8 --mpi 1.1.1.8
+echo "==== Completed Test on 8 GPUs ===="
 
 echo "End Test: `date`"
