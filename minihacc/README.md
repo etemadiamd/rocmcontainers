3e7a1602b8fa89c5725bb57a64292fe86e4ef68f
Roohollah Etemadi <roohollah.etemadi@amd.com>
2023-01-19 16:37:37 +0000

    updated minihacc docker tag to 1.0.amd3_129

--- minihacc/README.md
+++ minihacc/README.md
@@ -1,6 +1,7 @@
 ```
 Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.
-Revision: V1.0
+Revision: V2.0
+V2.0: update for new image minihacc:1.0.amd3_129
 V1.0: add docker and singualrity commands
 ```
 # Mini-Hacc
@@ -8,25 +9,25 @@ V1.0: add docker and singualrity commands
 ## Pull Command
 
 ```
-sudo docker pull amdih/minihacc:1.0.0
+sudo docker pull amdih/minihacc:1.0.amd3_129
 ```
 ## Running Containers
 There are two benchmarks: fp32 and fp64 which can be ran using following commands:
 
 FP32:
 ```
-sudo docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/minihacc:1.0.0 mini-HACC
+sudo docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/minihacc:1.0.amd3_129 mini-HACC
 ```
 FP64:
 ```
-sudo docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/minihacc:1.0.0 mini-HACC-fp64
+sudo docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/minihacc:1.0.amd3_129 mini-HACC-fp64
 ```
 ## Run Using Singularity
 This section assumes that an up-to-date version of Singularity is installed on your system and properly configured for your system. Please consult with your system administrator or view official Singularity documentations.
 
 Pull and convert docker image to singularity image format:
 ```
-singularity pull minihacc.sif docker://amdih/minihacc:1.0.0
+singularity pull minihacc.sif docker://amdih/minihacc:1.0.amd3_129
 ```
 You can then use examples from the preceding section to use the image. To run FP32 benchmark:
 ```
--- minihacc/run_minihacc_singularity.sh
+++ minihacc/run_minihacc_singularity.sh
@@ -4,25 +4,29 @@
 # Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.                        
 # This script is used to run AMD Infiniy Mini-Hacc singularity commands                            
 # Contact info: roohollah.etemadi@amd.com                                                      
-# Version: V1.0                                                                                  
-# Modified: 2022-05-16                                                                          
-# Version History:                                                                              
+# Version: V2.0                                                                                  
+# Modified: 2023-01-19                                                                          
+# Version History:    
+# V2.0: run amddcgpuce/minihacc:1.0.amd3_129 singularity commands
 # V1.0: run amdih/minihacc:1.0.0 singularity commands                                   
 #************************************************************************************************
 echo "Start Test: `date`"
 
+docker_tag=amddcgpuce/minihacc:1.0.amd3_129
+singularity_tag=minihacc_1.0.amd3_129.sif
+
 echo "==== Pull singularity image ===="
-echo "singularity pull minihacc.sif docker://amdih/minihacc:1.0.0"
-singularity pull minihacc.sif docker://amdih/minihacc:1.0.0
+echo "singularity pull $singularity_tag docker://$docker_tag"
+singularity pull $singularity_tag docker://$docker_tag
 echo "==== Pull complete ===="
 
-echo "==== Run FP32 Test ===="
-echo "singularity run ./minihacc.sif mini-HACC"
-singularity run ./minihacc.sif mini-HACC
-echo "==== Complete FP32 Test ===="
+echo "==== Run FP32 Test `date`===="
+echo "singularity run ./$singularity_tag mini-HACC"
+singularity run ./$singularity_tag mini-HACC
+echo "==== Complete FP32 Test `date`===="
 
-echo "==== Run FP64 Test ===="
-echo "singularity run ./minihacc.sif mini-HACC-fp64"
-singularity run ./minihacc.sif mini-HACC-fp64
-echo "==== Complete FP64 Test ===="
+echo "==== Run FP64 Test `date`===="
+echo "singularity run ./$singularity_tag mini-HACC-fp64"
+singularity run ./$singularity_tag mini-HACC-fp64
+echo "==== Complete FP64 Test `date`===="
 echo "End Test: `date`"
